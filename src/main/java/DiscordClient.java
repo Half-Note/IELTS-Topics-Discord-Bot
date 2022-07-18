b1732764c8059facc6a0fa853b96a18d446dd365
BenderBotRepo <105135037+BenderBotRepo@users.noreply.github.com>
2022-07-18 22:57:18 +0800

    Update dependencies to support text in voice channels

--- src/main/java/DiscordClient.java
+++ src/main/java/DiscordClient.java
@@ -1,6 +1,7 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
 
@@ -9,6 +10,7 @@ public class DiscordClient {
 
     public static void main(String[] args) throws LoginException, InterruptedException {
         JDA jda = JDABuilder.createDefault(TOKEN)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .addEventListeners(
                         new Ping(),
                         new Help(),
